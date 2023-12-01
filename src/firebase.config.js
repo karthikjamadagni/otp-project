a15b60b5a1ff4608d3db4c9df01af92dcf7e42d1
karthikjamadagni <108860905+karthikjamadagni@users.noreply.github.com>
2023-12-01 17:12:29 +0530

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyAA8aoLDuDpslD1wlyyp6G4Kh7kq2Ufk-8",
+  authDomain: "new-otp-project-b67a6.firebaseapp.com",
+  projectId: "new-otp-project-b67a6",
+  storageBucket: "new-otp-project-b67a6.appspot.com",
+  messagingSenderId: "898092361920",
+  appId: "1:898092361920:web:6b3584613f56fe6f542525",
+  measurementId: "G-X2Q7KZENML"
 };
 
 // Initialize Firebase
